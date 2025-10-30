1ee325eec255d972f86c333ed4810fc12d765794
Lucas <lucasbaarbosaa4@gmail.com>
2025-10-30 18:01:29 -0300

    mentoria e curso

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso Java");
+        curso1.setCarga(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Curso Java");
+        curso2.setCarga(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+    }
+}
---
+++ src/main/java/br/com/desafio/dominio/Curso.java
@@ -0,0 +1,43 @@
+package br.com.desafio.dominio;
+
+public class Curso {
+    private String titulo;
+    private String descricao;
+    private int carga;
+
+    public Curso() {
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", carga=" + carga +
+                '}';
+    }
+}
